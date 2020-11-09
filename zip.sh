2ab69bd8c5e15312e48a8f91d5e633cd49a1baa4
Angel Virgen <virgen@chapman.edu>
2020-11-09 10:44:24 -0800

    Adds username.sh

--- zip.sh
+++ zip.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
-# zip.sh
-# author
-echo "Enter a five-digit ZIP code: "
+# username.sh
+# Angel Virgen
+echo "Enter a username: "
+echo "Use lower case characters, digits and underscore"
+echo " It must start with a lower case character"
+echo " It must contain at least 3 but not more than 12 characters"
+
 read ZIP
 while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
 do
